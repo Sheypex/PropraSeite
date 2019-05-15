4fb51e158a5503f7c7621cd25e4a9515b9116da3
sheypex <sheypex@gmail.com>
2019-05-15 11:51:31 +0200

    visuelles und informatives

--- app/moreInfoView/moreInfoView.spec.js
+++ app/moreInfoView/moreInfoView.spec.js
@@ -1,10 +1,10 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('myApp.moreInfoView module', function() {
 
-  beforeEach(module('myApp.view2'));
+  beforeEach(module('myApp.moreInfoView'));
 
-  describe('view2 controller', function(){
+  describe('moreInfoView controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
