ab413739c0029d3c4a7f07aeaccd7ffa5e3f5722
acuna <neuimDAI23>
2019-06-24 13:06:27 +0200

    Graph plotter js now generates also a sentiment graph. Added TODO to mark where the loading should be done

--- server/DataAnalisis/term_top_users_loader.py
+++ server/DataAnalisis/term_top_users_loader.py
@@ -8,7 +8,6 @@ def load_terms_topuser_file(path):  # loads the file, sorts it and builds a json
     result = pd.read_csv(path, sep=";", error_bad_lines=False)
     result = result[['User', 'Count']].copy()
     result = result[::-1]
-    #  print result.to_json(orient='values')
     return result
 
 
